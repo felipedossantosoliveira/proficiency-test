1f9036d87f2881ddc33dbfc374bea9ee4b5c9ad0
felipe <felipedossantosads@gmail.com>
2024-05-12 18:39:26 -0300

    feat: adicionando relacionamento de produto na model de cliente

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
@@ -35,4 +36,11 @@ class Customer extends Model
         return $this->belongsTo(City::class);
     }
 
+    /**
+     * @return HasMany
+     */
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
